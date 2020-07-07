0cea9b199bd4745ef388dfd20acece5b76c65204
Kevin Botero <kbotero201@gmail.com>
2020-07-07 23:27:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element 
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -23,8 +23,9 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index(array, index_number)
+  array[0]
 end
+
 
 def retrieve_first_element_from_array(array)
   array[0]
